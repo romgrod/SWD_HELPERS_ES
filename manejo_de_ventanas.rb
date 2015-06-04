d475171762734d4dd612cdd308f084af7e2a1a3d
Roman Rodriguez <roman.g.rodriguez@gmail.com>
2015-06-04 16:00:08 -0300

    first commit

---
+++ manejo_de_ventanas.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+def titulo_de_ventana
+    @browser.title
+  end
+
+# Devuelve un array con todos los ids de las ventanas que están abiertas
+def ventanas_abiertas
+  @browser.window_handles
+end
+
+def saltar_a_ventana id
+  @browser.switch_to.window(id)
+end
+
+def saltar_a_la_ultima_ventana_abierta
+  self.saltar_a_ventana(ventanas_abiertas.last)
+end
+
+def saltar_a_ventana_con_titulo titulo
+  ventanas_abiertas.each do |id|
+    if titulo_de_ventana == titulo
+      saltar_a_ventana(id)
+      return true # Devuelve true si encontró la ventana
+    end
+  end
+  false # NO encontró la ventana
+end
